4762c898b0fb5336a3bebec045cca0439ba8f01c
김용해 <yoho1019@gmail.com>
2025-04-20 15:52:56 +0900

    UI 작업 끝

--- PlaygroundSwiftUI/views/GithubUserListView.swift
+++ PlaygroundSwiftUI/views/GithubUserListView.swift
@@ -9,22 +9,52 @@ import SwiftUI
 
 struct GithubUserListView: View {
     @StateObject private var viewModel: GithubListViewModel = .init()
-    @State private var searchText: String = ""
     var body: some View {
-        List {
-            githubRow
+        ZStack {
+            if viewModel.users.isEmpty {
+                Text("검색 해볼까요 ✅")
+                    .font(.title2).bold()
+            }else {
+                List {
+                    ForEach(viewModel.users) { user in
+                        githubRow(user: user)
+                    }
+                }
+            }
+            
+            if viewModel.isLoading {
+                ProgressView()
+            }
+            
+            if let errorMessage = viewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+                    .background(.gray.opacity(0.2))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            }
         }
-        .searchable(text: $searchText, prompt: "GitHub 사용자 검색")
+        .searchable(text: $viewModel.searchText, prompt: "GitHub 사용자 검색")
         .navigationTitle("깃헙 사용자 검색")
-
     }
     
     // MARK: 깃헙 사용자 목록 Cell
-    private var githubRow: some View {
+    private func githubRow(user: GithubUser) -> some View {
         HStack {
-            Image(systemName: "person.circle")
-                .font(.largeTitle)
-            Text("123")
+            AsyncImage(url: URL(string: user.avatarUrl)) { image in
+                image
+                .resizable()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+            } placeholder: {
+                Image(systemName: "person.circle.fill") // Placeholder 이미지
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .foregroundColor(.gray)
+            }
+                
+            Text(user.username)
                 .font(.title2).bold()
         }
         .padding(.vertical)
