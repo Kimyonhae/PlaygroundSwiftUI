9d41ac6665a70da118dc3c54f5884657d921cddb
김용해 <yoho1019@gmail.com>
2025-04-20 15:52:36 +0900

    XCTest 테스트 코드 작성 및 디버깅을 위한 테스트 함수 생성

--- PlaygroundSwiftUITests/GithubSearchList.swift
+++ PlaygroundSwiftUITests/GithubSearchList.swift
@@ -13,7 +13,19 @@ final class GithubSearchList: XCTestCase {
     func testExamplePlus() {
         let a: Int = 1
         let b: Int = 2
-        
         XCTAssertEqual(a + b, 3)
+    }
+    
+    func testDebugPrint() {
+        let description = self.expectation(description: "GITHUB API 기다리기")
+        let viewModel = GithubListViewModel()
+        print("---------- DEBUG PRINT ------------")
+        viewModel.fetchGithubUsers(username: "") { isSuccess in
+            print("코드가 통과되었는지 확인합니다 isSuccess : \(isSuccess)")
+            description.fulfill()
+        }
+        wait(for: [description], timeout: 5)
+        print("-------- DEBUG PRINT END ----------")
+        XCTAssertTrue(true)
     }
 }
