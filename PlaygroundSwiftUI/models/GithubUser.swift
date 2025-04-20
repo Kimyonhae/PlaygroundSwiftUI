c2cfcd65b86bdb2af77cf64269909c7e19660e1c
김용해 <yoho1019@gmail.com>
2025-04-20 15:51:05 +0900

    API 모델링 생성

--- PlaygroundSwiftUI/models/GithubUser.swift
+++ PlaygroundSwiftUI/models/GithubUser.swift
@@ -4,10 +4,23 @@
 //
 //  Created by 김용해 on 4/19/25.
 //
+import Foundation
 
 // TODO: Github에게 가져올 API 내용 모델링
-struct GithubUser {
+struct SearchResponse: Codable {
+    let total_count: Int
+    var items: [GithubUser]
+}
+
+
+struct GithubUser: Codable, Identifiable {
     let id: UInt32
     let username: String
-    let avatar_url: String
+    let avatarUrl: String
+    
+    enum CodingKeys: String ,CodingKey {
+        case id
+        case username = "login"
+        case avatarUrl = "avatar_url"
+    }
 }
