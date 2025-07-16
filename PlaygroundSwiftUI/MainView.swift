ab1034afcb9fe6df257ac394106827a02e9595c8
김용해 <yoho1019@gmail.com>
2025-07-16 22:56:15 +0900

    MainView에 Coordinator 패턴 적용 및 Auth 페이지 추가

--- PlaygroundSwiftUI/MainView.swift
+++ PlaygroundSwiftUI/MainView.swift
@@ -26,19 +26,22 @@ let routes: [ConnectionView] = [
     ConnectionView(title: "AnimatedList Playground", destination: AnimatedList()),
     ConnectionView(title: "LandMarks Playground", destination: LandMarks()),
     ConnectionView(title: "GithubUserListView Playground", destination: GithubUserListView()),
-    ConnectionView(title: "Subscriber Playground", destination: SubscriberView(title: "구독 연습"))
+    ConnectionView(title: "Subscriber Playground", destination: SubscriberView(title: "구독 연습")),
 ]
 
 struct MainView: View {
+    @EnvironmentObject var coordinator: Coordinator
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(routes) { route in
-                    NavigationLink(destination: route.destination) {
-                        Text(route.title)
-                    }
+        List {
+            ForEach(routes) { route in
+                NavigationLink(destination: route.destination) {
+                    Text(route.title)
                 }
             }
+            Text("Auth in Coordinator Playground")
+                .onTapGesture {
+                    coordinator.push(.login)
+                }
         }
         .navigationTitle("Swift Playground")
     }
@@ -49,3 +52,9 @@ struct MainView: View {
         MainView()
     }
 }
+
+/// ** 회고 **
+/// Coordinator 패턴적용은 앱 시작부분에서 하는게 정석인거 같다.
+/// 기존 뷰 로직을 바꾸지 않고 할려면  Coordinator ->( 기존 Main 뷰 , 새로 추가한 Page)
+/// 결론은 하나의 줄기에서 시작해야 하고 Coordinator 사용할려면 중간에 분기가 나눠지면 안됌.
+///
