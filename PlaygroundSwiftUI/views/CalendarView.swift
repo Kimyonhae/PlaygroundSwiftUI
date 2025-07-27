572aa9ae36a5b8c9c81eccfc6abf959876ea3f96
김용해 <yoho1019@gmail.com>
2025-07-27 15:30:28 +0900

    달력 뷰 개선: 선택 섹션 추가 및 날짜 선택 로직 구현 각 SectionType에 따라 뷰 로직이 바뀌는 구조

--- PlaygroundSwiftUI/views/CalendarView.swift
+++ PlaygroundSwiftUI/views/CalendarView.swift
@@ -17,26 +17,68 @@ struct DayCell: Identifiable {
     }
 }
 
+
+
 struct CalenderView: View {
     @State private var date = Date.now
     @State private var color: Color = .blue
+    
+    @State private var generalSelectDay: Date? = nil
+    @State private var connectDates: (start: Date, end: Date)? = nil
+    @State private var connectFirstDayTaped: Date? = nil
+    @State private var multipleDates: [Date]? = nil
+    @State private var selectSection: SectionType = .general
+    
     let columns = Array(repeating: GridItem(.flexible()), count: 7)
     var body: some View {
         VStack {
+            Picker("", selection: $selectSection) {
+                ForEach(SectionType.allCases, id: \.self) { section in
+                    Text(section.displayName).tag(section)
+                }
+            }
+            .pickerStyle(.segmented)
             headerView
             middleDays
             daysView
         }
         .padding()
+        .onChange(of: selectSection) {
+            switch selectSection {
+                case .general:
+                    connectDates = nil
+                    multipleDates = nil
+                    connectFirstDayTaped = nil
+                case .connect:
+                    generalSelectDay = nil
+                    multipleDates = nil
+                case .multiple:
+                    generalSelectDay = nil
+                    connectDates = nil
+                    connectFirstDayTaped = nil
+            }
+        }
     }
     
     // TODO: 상단 현재 year, month 및 색상 뷰
     private var headerView: some View {
         HStack {
             Group {
-                Button("", systemImage: "arrowshape.left.fill"){}
+                Button("", systemImage: "arrowshape.left.fill") {
+                    if let prev = Calendar.current.date(byAdding: .month, value: -1, to: date) {
+                        withAnimation(.smooth) { 
+                            date = prev
+                        }
+                    }
+                }
                 Text(getDayOfWeek(date))
-                Button("", systemImage: "arrowshape.right.fill"){}
+                Button("", systemImage: "arrowshape.right.fill") {
+                    if let next = Calendar.current.date(byAdding: .month, value: 1, to: date) {
+                        withAnimation(.smooth) {
+                            date = next
+                        }
+                    }
+                }
             }
             .font(.title)
             .lineLimit(1)
@@ -54,6 +96,7 @@ struct CalenderView: View {
             ForEach(weekdays, id: \.self) { day in
                 Text(day)
                     .padding()
+                    .foregroundStyle(color)
             }
         }
     }
@@ -62,16 +105,13 @@ struct CalenderView: View {
     private var daysView: some View {
         LazyVGrid(columns: columns) {
             ForEach(generateDaysInMonth(from: date)) { cell in
-                if let date = cell.date {
-                    Text("\(Calendar.current.component(.day, from: date))")
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical)
-                        .background(color)
-                        .foregroundStyle(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
-                        .id(cell.id)
-                } else {
-                    Text("")
+                switch selectSection {
+                case .general:
+                    generalDayCell(cell)
+                case .connect:
+                    connectDayCell(cell)
+                case .multiple:
+                    multipleDayCell(cell)
                 }
             }
         }
@@ -84,6 +124,31 @@ extension CalenderView {
     }
 }
 
+// DataStruct Define
+extension CalenderView {
+    enum SectionType: Equatable {
+        case general
+        case connect
+        case multiple
+
+
+        var displayName: String {
+            switch self {
+            case .general: return "일반"
+            case .connect: return "연속 선택"
+            case .multiple: return "다수 선택"
+            }
+        }
+
+        static var allCases: [SectionType] {
+            [
+                .general,
+                .connect,
+                .multiple
+            ]
+        }
+    }
+}
 
 extension CalenderView {
     private func getDayOfWeek(_ date: Date) -> String {
@@ -101,7 +166,7 @@ extension CalenderView {
         
         guard let monthRange = calendar.range(of: .day, in: .month, for: date),
               let firstDay = calendar.date(from: calendar.dateComponents([.year, .month], from: date)) else {
-                  return []
+            return []
         }
         
         let firstWeekday = calendar.component(.weekday, from: firstDay)
@@ -115,7 +180,87 @@ extension CalenderView {
         
         return days
     }
-}
-#Preview {
-    CalenderView()
+    
+    // TODO: 일반 뷰빌더 로직
+    @ViewBuilder
+    private func generalDayCell(_ cell: DayCell) -> some View {
+        if let date = cell.date {
+            Text("\(Calendar.current.component(.day, from: date))")
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                .background(generalSelectDay == date ? .red : color)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                .id(cell.id)
+                .onTapGesture {
+                    generalSelectDay = date
+                }
+        } else {
+            Text("")
+        }
+    }
+    
+    // TODO: 연속 선택 뷰빌더 로직
+    @ViewBuilder
+    private func connectDayCell(_ cell: DayCell) -> some View {
+        if let date = cell.date {
+            
+            let isSelected: Bool = {
+                guard let range = connectDates else { return false }
+                return date >= range.start && date <= range.end
+            }()
+            
+            let isFirst = date == connectFirstDayTaped
+            Text("\(Calendar.current.component(.day, from: date))")
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                .background(isSelected || isFirst ? .red :  color)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                .id(cell.id)
+                .onTapGesture {
+                    if let firstDate = connectFirstDayTaped {
+                        connectDates = (firstDate, date)
+                        connectFirstDayTaped = nil
+                    }else {
+                        connectFirstDayTaped = date
+                        connectDates = nil
+                    }
+                }
+        } else {
+            Text("")
+        }
+    }
+    
+    // TODO: 불연속 뷰빌더 로직
+    @ViewBuilder
+    private func multipleDayCell(_ cell: DayCell) -> some View {
+        if let date = cell.date {
+            let isSelected: Bool = {
+                guard let dates = multipleDates else { return false }
+                return dates.contains(date)
+            }()
+            
+            Text("\(Calendar.current.component(.day, from: date))")
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                .background(isSelected ? .red : color)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                .id(cell.id)
+                .onTapGesture {
+                    if isSelected {
+                        multipleDates?.removeAll { $0 == date }
+                    } else {
+                        if multipleDates == nil {
+                            multipleDates = [date]
+                        } else {
+                            multipleDates?.append(date)
+                        }
+                    }
+                }
+        } else {
+            Text("")
+        }
+    }
 }
