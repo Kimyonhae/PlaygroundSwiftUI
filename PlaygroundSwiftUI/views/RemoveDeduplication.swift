bc66fc466b3375bd3d7d39c69d8fad35f2d8f93f
김용해 <yoho1019@gmail.com>
2025-08-01 17:27:34 +0900

    상위뷰에서 자식의 Size를 알아오는 방법 PreferenceKey

--- PlaygroundSwiftUI/views/RemoveDeduplication.swift
+++ PlaygroundSwiftUI/views/RemoveDeduplication.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct RemoveDeduplication: View {
     @State private var isPresented: Bool? = nil
+    @State private var size: CGSize = .zero
     var body: some View {
         VStack {
             Button("sheet 올리기") {
                 isPresented = false
             }
+            HStack {
+                Text("크기 측정")
+                    .readSize { size = $0 }
+                    
+            }
+            Text("부모가 자식의 크기를 알아내는 방법 : \(size)")
         }
         .sheet(isPresented: $isPresented.presence()) {
             Text("hello Sheet")
         }
+    }
+}
+
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
+
+extension View {
+    func readSize(completion: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geo in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geo.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self, perform: completion)
     }
 }
 
